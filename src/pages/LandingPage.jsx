ddeb79e1be63bdc65476e32fbe427144cf6e44bb
TS <tinusunny04@gmail.com>
2025-11-10 00:49:09 +0530

    landingpage update with about and  4 Steps and Eligibility pending

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,11 @@
-import { Box, Container, Typography, Button, Stack } from "@mui/material";
+import { Box, Container, Typography, Button, Stack, Grid } from "@mui/material";
+import * as React from 'react';
+import { Card, CardContent } from '@mui/material';
+// import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+
 
 import Header from '../components/Header'
+import '../App.css'
 
 function LandingPage() {
   return (
@@ -14,9 +19,10 @@ function LandingPage() {
       <Container
         maxWidth="lg"
         sx={{
-          py: 16,
+          py: 5,
           px: { xs: 2, sm: 4, lg: 6 },
           textAlign: "center",
+          mt:4
         }}
       >
         {/* Text Section */}
@@ -123,6 +129,281 @@ function LandingPage() {
         </Box>
       </Container>
     </main>
+
+{/*  section 2 */}
+    <section>
+
+      <Container maxWidth='lg'
+      sx={{textAlign:"center"}}>
+<Typography variant="h3"
+sx={{fontWeight:"800",fontSize:{xs:'1.5rem ',md:"3rem"} }}> Your Life-Saving Journey in 4 Steps </Typography>
+<Typography variant="h6"
+sx={{maxWidth:'lg',m:4,color:'text.secondary', fontSize:{xs:'1rem ',md:"1.4rem"} }}>The entire process usually takes less than an hour, and your safety is our priority.</Typography>
+
+
+
+{/* steo boxs */}
+        <Stack 
+        direction={{xs:"column",sm:"row"}}
+        spacing={{xs:2 ,sm:3,lg:2}}
+        justifyContent="space-between"
+        alignItems="center"
+        margin={{xs:0,sm:2 ,lg:4,md:5}}
+        width='100%'
+        >
+          <Box 
+          sx={{
+            display:'flex',
+            justifyContent:'center',
+            alignItems:"center",
+           transition: '0.3s',
+        '&:hover': {
+          boxShadow: 4,},
+          backgroundColor:"white",
+            width:290,
+            height:180,
+             boxShadow: 1,
+            p:2,
+            borderRadius:2,
+             borderTop: '8px solid #d32f2f'
+          }}>
+
+             <CardContent sx={{ p: 1 }}>
+           
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ fontWeight: 600, mb: 2 }}
+        >
+          1. Register
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Sign in and complete your consent and health history forms.
+        </Typography>
+      </CardContent>
+            
+          </Box>
+
+
+          <Box 
+          sx={{
+            display:'flex',
+            justifyContent:'center',
+            alignItems:"center",
+           transition: '0.3s',
+        '&:hover': {
+          boxShadow: 4,},
+          backgroundColor:"white",
+            width:290,
+            height:180,
+             boxShadow: 1,
+            p:2,
+            borderRadius:2,
+             borderTop: '8px solid #d32f2f'
+          }}>
+
+             <CardContent sx={{ p: 1 }}>
+           
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ fontWeight: 600, mb: 2 }}
+        >
+         2. Screening
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        A private check of your blood pressure, pulse, temperature, and iron level.
+        </Typography>
+      </CardContent>
+            
+          </Box>
+
+          <Box 
+          sx={{
+            display:'flex',
+            justifyContent:'center',
+            alignItems:"center",
+           transition: '0.3s',
+        '&:hover': {
+          boxShadow: 4,},
+          backgroundColor:"white",
+            width:290,
+            height:180,
+             boxShadow: 1,
+            p:2,
+            borderRadius:2,
+             borderTop: '8px solid #d32f2f'
+          }}>
+
+             <CardContent sx={{ p: 1 }}>
+           
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ fontWeight: 600, mb: 2 }}
+        >
+         3. Donate
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        The actual blood collection process typically takes only 8-10 minutes.
+        </Typography>
+      </CardContent>
+            
+          </Box>
+
+
+          <Box 
+          sx={{
+            display:'flex',
+            justifyContent:'center',
+            alignItems:"center",
+           transition: '0.3s',
+        '&:hover': {
+          boxShadow: 4,},
+          backgroundColor:"white",
+            width:290,
+            height:180,
+             boxShadow: 1,
+            p:2,
+            borderRadius:2,
+             borderTop: '8px solid #d32f2f'
+          }}>
+
+             <CardContent sx={{ p: 1 }}>
+           
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ fontWeight: 600, mb: 2 }}
+        >
+       4. Relax
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        Enjoy complimentary snacks and drinks for 10-15 minutes before leaving.
+        </Typography>
+      </CardContent>
+            
+          </Box>
+
+
+          
+        </Stack>
+
+      </Container>
+    </section>
+
+
+    {/* About  */}
+
+<Container>
+      <Typography
+        variant="h3"
+        sx={{
+          textAlign: "center",
+          fontWeight: 800,
+          mt: 5,
+          color: "error.main",
+        }}
+      >
+        About Us
+      </Typography>
+
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        justifyContent="space-evenly"
+        alignItems="center"
+        spacing={4}
+        sx={{
+          my: { xs: 2, sm: 4, md: 6 },
+          width: "100%",
+        }}
+      >
+        {/* Left Text Section */}
+        <Box sx={{ flex: 1 }}>
+          <Typography variant="h4" sx={{ fontWeight: 600, mb: 2 }}>
+            Life Drop
+          </Typography>
+
+          <Typography variant="body1" color="text.secondary">
+            Life Drop is a dedicated platform that bridges the gap between blood
+            donors and patients in need. Our goal is to make blood donation
+            faster, simpler, and more accessible for everyone, ensuring that no
+            life is lost due to a lack of blood availability.
+          </Typography>
+
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            marginTop="20px"
+          >
+            Through Life Drop, donors can easily register, maintain their health
+            records, and be notified when their donation can make a real
+            difference. We partner with hospitals, blood banks, and
+            organizations to create a safe, transparent, and reliable donation
+            ecosystem. Every drop counts — and with your help, we can save
+            countless lives.
+          </Typography>
+        </Box>
+
+        {/* Right Image Section */}
+        <Box
+          sx={{
+            flex: 1,
+            display: "flex",
+            justifyContent: "center",
+            //  borderTop: '8px solid #ce0808d9',
+              maxWidth: "400px",
+             borderRadius:2
+          }}
+        >
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlHstR2_zfdTQp5AHYqx3hb3qfrVMZ2HVNmw&s"
+            alt="Life Drop"
+            style={{
+              width: "100%",
+              maxWidth: "400px",
+              borderRadius: "8px",
+              objectFit: "cover",
+              boxShadow: "0px 4px 12px rgba(248, 9, 9, 0.82)", 
+            }}
+          />
+        </Box>
+      </Stack>
+    </Container>
+
+{/* Eligibility Check  */}
+    <Container>
+
+      <Typography variant="h3" color="error.main" sx={{ textAlign:'center', fontWeight:800, margin:5}}>
+        Eligibility
+      </Typography>
+
+      <Box sx={{ boxShadow:5 ,p:4, borderRadius:5, textAlign:'center'}}>
+        <Stack >
+          <Typography variant="h4" sx={{fontWeight:800, textAlign:'left'}}>
+            New Donor? Check Your Eligibility First!
+          </Typography>
+          <Typography variant="p" sx={{textAlign:'left'}}> Quickly review our official guidelines regarding age, weight, and recent travel to ensure you can donate safely.</Typography>
+        </Stack>
+        <Button  variant="contained"  size="large"  sx={{bgcolor: "error.main",
+                color: "white",
+                px: 5,
+                py: 1.5,
+                fontWeight: "bold",
+                borderRadius: "50px",
+                textTransform: "uppercase",
+                letterSpacing: "0.08em",
+                boxShadow: "0px 8px 24px rgba(255, 0, 0, 0.4)",
+                "&:hover": {
+                  bgcolor: "error.dark",
+                  transform: "scale(1.05)",
+                  boxShadow: "0px 10px 30px rgba(255, 0, 0, 0.6)",
+                },
+                transition: "all 0.3s ease",
+                }}>View Donor Guidelines</Button>
+      </Box>
+
+    </Container>
 
 
 
